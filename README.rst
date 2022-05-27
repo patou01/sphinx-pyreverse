8ff5fc9408375850f368bda0298d3810f4dd174b
patou01 <2592673+patou01@users.noreply.github.com>
2022-05-27 07:35:13 +0200

    Make local arguments work like the CLI, maybe (#1)
    * Update readme
    * Arguments passed at call-time now are used instead of throwing
    * Works for my use-case for now
    * added pre-commit
    * removed bunch of test configs that I'm too lazy to setup
    * removed empty doc

--- README.rst
+++ README.rst
@@ -1,20 +1,22 @@
 Sphinx-pyreverse
 =================
 
-.. image:: https://circleci.com/gh/alendit/sphinx-pyreverse.svg?style=svg
-    :target: https://circleci.com/gh/alendit/sphinx-pyreverse
+**Note: This is not what you will get by doing pip install sphinx-pyreverse.** This has been forked. And for now there
+are no plans of merging back.
 
-.. image:: https://badge.fury.io/py/sphinx-pyreverse.svg
-    :target: https://badge.fury.io/py/sphinx-pyreverse
+**No tests are run on this code. I modified decently large amounts to suit my need for one project and do not care
+about other uses. If you find a bug or have a question, feel free to ask! If there seems to be interest in this package
+I might write some proper doc and tests**
 
 A simple sphinx extension to generate a UML diagram from python modules.
 
 Install
 --------
 
-Install with:::
+Install should work with: ::
 
-    pip install sphinx-pyreverse
+    pip install https://github.com/patou01/sphinx-pyreverse
+
 
 Usage
 ------
@@ -22,23 +24,34 @@ Usage
 Add "sphinx_pyreverse" to the extensions list in your conf.py (make sure it is
 in the PYTHONPATH).
 
-Call the directive with path to python module as content. The ``:classes:`` and
-``:packages:`` flags specify which UML diagrams to show.::
+Call the directive with path to python module as content. ::
 
-    .. uml:: {{modulename}}
-        :classes:
-        :packages:
+    .. uml:: my_module
+        :classes: MyClass
 
 Requires pyreverse from pylint.
+
+Arguments
+^^^^^^^^^
+
+It should support the standard arguments from pyreverse, but I honestly haven't tested. For instance you can do: ::
+
+    .. uml:: my_module
+        :classes: MyClass
+        :all-ancestors:
+
+And that should run and give you what you're after...
+
+**Note: depending on your basic config, it will not override nor detect conflict situations. For instance if you put**
+``all-ancestors`` **in the default and then add** ``show-ancestors`` **in the call to uml, you will get a command containing both**
 
 Options
 ^^^^^^^
 
-To configure usage, in your conf.py
+You can write default config to `conf.py`, if an override is found in the arguments, that one will have precedence.
 
 * ``sphinx_pyreverse_output`` (see --output), default is "png"
 * ``sphinx_pyreverse_filter_mode`` (see --filter_mode), default is None
-* ``sphinx_pyreverse_class`` (see --class), default is None
 * ``sphinx_pyreverse_show_ancestors`` (see --show_ancestors), default is None
 * ``sphinx_pyreverse_all_ancestors`` (see --all_ancestors), default is None
 * ``sphinx_pyreverse_show_associated`` (see --show_associated), default is None
@@ -63,8 +76,8 @@ To use it run your ``sphinx-build`` command with ``-v -v -v -w $(pwd)/sphinx.log
 
 For more information see:
 
-  * `-v switch`_ 
-  * `-w switch`_ 
+  * `-v switch`_
+  * `-w switch`_
 
 
 .. _-v switch: https://www.sphinx-doc.org/en/master/man/sphinx-build.html#cmdoption-sphinx-build-v
--- sphinx_pyreverse/uml_generate_directive.py
+++ sphinx_pyreverse/uml_generate_directive.py
@@ -8,7 +8,7 @@ First, Created on Oct 1, 2012, this file created on Oct 8, 2019
 
 import copy
 import os
-import subprocess
+import subprocess  # nosec
 import sys
 
 from docutils import nodes
@@ -34,30 +34,61 @@ except ImportError:  # pragma: no cover
 
 def subproc_wrapper(*args, **kwargs):  # pragma: no cover
     """A shim which allows mocking of the subproc call when using pytest"""
-    subprocess.check_output(*args, **kwargs)
+    subprocess.check_output(*args, **kwargs)  # nosec
 
 
 class UMLGenerateDirective(Directive):
     """UML directive to generate a pyreverse diagram"""
 
     required_arguments = 1
-    optional_arguments = 2
+    optional_arguments = 8
     has_content = False
     DIR_NAME = "uml_images"
     # a list of modules which have been parsed by pyreverse
     generated_modules = []
 
-    def _validate(self):
-        """Validates that the RST parameters are valid"""
-        valid_flags = {":classes:", ":packages:"}
-        unkown_arguments = set(self.arguments[1:]) - valid_flags
-        if unkown_arguments:
-            raise ValueError(
-                (
-                    f"invalid flags encountered: {unkown_arguments}. "
-                    f"Must be one of {valid_flags}"
-                )
-            )
+    def _add_local_arguments(self):
+        """overwrites arguments from command if necessary"""
+        cmd_name = ""
+        for arg in self.arguments[1:]:
+            if arg.startswith(":") and arg.endswith(":"):
+                name = arg.replace(":", "")
+                cmd_name = self._rst_to_cmd_arg(name)
+                self.command_args[cmd_name] = []
+            else:
+                self.command_args[cmd_name].append(arg)
+
+        for key, val in self.command_args.items():
+            if val:
+                self.command_args[key] = " ".join(val)
+
+    @staticmethod
+    def _rst_to_cmd_arg(name):
+        if name == "classes":
+            return "--class"
+
+        return f"--{name}"
+
+    def _get_command_args_from_config(self, config):
+        self.command_args = {}
+        if config.sphinx_pyreverse_filter_mode:
+            self.command_args["--filter-mode"] = config.sphinx_pyreverse_filter_mode
+        if config.sphinx_pyreverse_show_ancestors:
+            self.command_args["--show-ancestors"] = config.sphinx_pyreverse_show_ancestors
+        if config.sphinx_pyreverse_all_ancestors:
+            self.command_args["--all-ancestors"] = True
+        if config.sphinx_pyreverse_show_associated:
+            self.command_args["--show-associated"] = config.sphinx_pyreverse_show_associated
+        if config.sphinx_pyreverse_all_associated:
+            self.command_args["--all-associated"] = True
+        if config.sphinx_pyreverse_show_builtin:
+            self.command_args["--show-builtin"] = True
+        if config.sphinx_pyreverse_module_names:
+            self.command_args["--module-names"] = config.sphinx_pyreverse_module_names
+        if config.sphinx_pyreverse_only_classnames:
+            self.command_args["--only-classnames"] = True
+        if config.sphinx_pyreverse_ignore:
+            self.command_args["--ignore"] = config.sphinx_pyreverse_ignore
 
     def _build_command(self, module_name, config):  # noqa: C901 func too-complex
         cmd = [
@@ -67,30 +98,16 @@ class UMLGenerateDirective(Directive):
             "--project",
             module_name,
         ]
-        if config.sphinx_pyreverse_filter_mode:
-            assert config.sphinx_pyreverse_filter_mode
-            cmd.extend(("--filter-mode", config.sphinx_pyreverse_filter_mode))
-        if config.sphinx_pyreverse_class:
-            cmd.extend(("--class", config.sphinx_pyreverse_class))
-        if config.sphinx_pyreverse_show_ancestors:
-            cmd.extend(("--show-ancestors", config.sphinx_pyreverse_show_ancestors))
-        if config.sphinx_pyreverse_all_ancestors:
-            cmd.append("--all-ancestors")
-        if config.sphinx_pyreverse_show_associated:
-            cmd.extend(("--show-associated", config.sphinx_pyreverse_show_associated))
-        if config.sphinx_pyreverse_all_associated:
-            cmd.append("--all-associated")
-        if config.sphinx_pyreverse_show_builtin:
-            cmd.append("--show-builtin")
-        if config.sphinx_pyreverse_module_names:
-            cmd.extend(("--module-names", config.sphinx_pyreverse_module_names))
-        if config.sphinx_pyreverse_only_classnames:
-            cmd.append("--only-classnames")
-        if config.sphinx_pyreverse_ignore:
-            cmd.extend(("--ignore", config.sphinx_pyreverse_ignore))
+
+        for key, value in self.command_args.items():
+            if key in ["--all-ancestors", "--all-associated", "--show-builtin", "--only-classnames"]:
+                cmd.append(key)
+            else:
+                cmd.extend([key, value])
 
         # finally append the module to generate the uml for
         cmd.append(module_name)
+
         return cmd
 
     def run(self):
@@ -108,13 +125,13 @@ class UMLGenerateDirective(Directive):
         env.uml_dir = uml_dir
         module_name = self.arguments[0]
 
-        self._validate()
+        self._get_command_args_from_config(env.config)
+
+        self._add_local_arguments()
 
         if module_name not in self.generated_modules:
             cmd = self._build_command(module_name, env.config)
-            logging.getLogger(__name__).info(
-                f"sphinx-pyreverse: Running: {' '.join(cmd)}"
-            )
+            logging.getLogger(__name__).info(f"sphinx-pyreverse: Running: {' '.join(cmd)}")
 
             # Ensure we have the right paths available to the pyreverse subproc
             if "PYTHONPATH" in os.environ:
@@ -139,29 +156,31 @@ class UMLGenerateDirective(Directive):
             self.generated_modules.append(module_name)
 
         res = []
-        for arg in self.arguments[1:]:
-            img_name = arg.strip(":")
-            res.append(
-                self.generate_img(img_name, module_name, base_dir, src_dir, env.config)
-            )
+        if "--class" in self.command_args:
+            # need to split on " " because the command has already been changed from arg[key] = ["A", "B"]
+            # to arg[key] = "A B"
+            for img_name in self.command_args["--class"].split(" "):
+                res.append(self.generate_img(img_name, module_name, base_dir, src_dir, env.config))
+        else:
+            # whatever this was doing...
+            for arg in self.arguments[2:]:
+                img_name = arg.strip(":")
+                res.append(self.generate_img(img_name, module_name, base_dir, src_dir, env.config))
 
         return res
 
     def get_paths(self, img_name, module_name, base_dir, src_dir, config):
-        path_from_base = os.path.join(self.DIR_NAME, "{1}_{0}.{2}").format(
-            module_name, img_name, config.sphinx_pyreverse_output
-        )
+        path_from_base = os.path.join(self.DIR_NAME, f"{img_name}.{config.sphinx_pyreverse_output}")
         # use relpath to get sub-directory of the main 'source' location
         src_base = os.path.relpath(base_dir, start=src_dir)
         uri = directives.uri(os.path.join(src_base, path_from_base))
         output_file = os.path.join(base_dir, path_from_base)
-        return (uri, output_file)
+
+        return uri, output_file
 
     def generate_img(self, img_name, module_name, base_dir, src_dir, config):
         """Resizes the image and returns a Sphinx image"""
-        (uri, output_file) = self.get_paths(
-            img_name, module_name, base_dir, src_dir, config
-        )
+        uri, output_file = self.get_paths(img_name, module_name, base_dir, src_dir, config)
         scale = 100
         max_width = 1000
         if IMAGE:
@@ -170,7 +189,5 @@ class UMLGenerateDirective(Directive):
             if image_width > max_width:
                 scale = max_width * scale / image_width
         else:
-            logging.getLogger(__name__).warning(
-                "sphinx-pyreverse: No image manipulation lib found!"
-            )
+            logging.getLogger(__name__).warning("sphinx-pyreverse: No image manipulation lib found!")
         return nodes.image(uri=uri, scale=scale)
